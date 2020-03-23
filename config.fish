419abad8713e5eec415659bb882690b8ed0b0eb5
Pierpaolo Frasa <pfrasa@gmail.com>
2020-03-23 20:01:32 +0100

    update config.fish

--- config.fish
+++ config.fish
@@ -1,11 +1,18 @@
-### FUNDLE
+########
+# FUNDLE
+########
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 fundle plugin 'edc/bass'
 fundle init
 
-###############
+########
+# CONFIG
+########
+set -x EDITOR /usr/bin/vim
+
+######
 # PATH
-###############
+######
 
 # clear path: this is a workaround for tmux setting paths twice
 set -e PATH
@@ -22,27 +29,35 @@ if test -d $HOME/.local/bin
   set -x PATH $HOME/.local/bin $PATH
 end
 
-if test -d $HOME/.rbenv
-  set -x PATH $HOME/.rbenv/bin $PATH
+if test -d $HOME/scripts
+  set -x PATH $HOME/scripts $PATH
 end
 
-if test -d $HOME/.pyenv
-  set -x PATH $HOME/.pyenv/bin $PATH
-  status --is-interactive; and source (pyenv init -|psub)
-end
+###########
+# Dev tools
+###########
 
-# direnv
+## DIRENV
 if test -f ".envrc"
   direnv allow .
 end
 
-######
-set -x EDITOR /usr/bin/vim
+## RUBY
+if test -d $HOME/.rbenv
+  set -x PATH $HOME/.rbenv/bin $PATH
+end
 
-# direnv
-eval (direnv hook fish)
+## PYTHON
+if test -d $HOME/.pyenv
+  set -x PATH $HOME/.pyenv/bin $PATH
+end
 
-# nvm
+## RUST
+if test -d $HOME/.cargo
+  set -x PATH $HOME/.cargo/bin $PATH
+end
+
+## NODE
 function nvm
   bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
@@ -55,25 +70,32 @@ function load_node
   end
 end
 
-if test -d ".nvm"
-  load_node
-end
-
 function __check_nvmrc --on-variable PWD --description "load per-project node version"
   load_node
 end
 
-# rbenv
-status --is-interactive; and . (rbenv init -|psub)
+## JVM
+function sdk
+  if test -d $HOME/.sdkman
+    bass source ~/.sdkman/bin/sdkman-init.sh --no-use ';' sdk $argv
+  else
+    echo "sdkman not installed"
+  end
+end
 
-#### ALIASES
+#########
+# ALIASES
+#########
 alias pull='git pull --rebase'
 alias push='git push origin (git rev-parse --abbrev-ref HEAD)'
 alias forcepush='git push -f origin (git rev-parse --abbrev-ref HEAD)'
+alias gamend='git commit --amend'
 
 alias fconf='vim ~/.config/fish/config.fish'
 
-#### FUNCTIONS
+##################
+# HELPER FUNCTIONS
+##################
 function to_gem
   cd (bundle show $argv[1])
 end
@@ -95,3 +117,22 @@ function fuck -d 'Correct your previous console command'
     history --delete $fucked_up_commandd
   end
 end
+
+##################
+# Initialize tools
+##################
+eval (direnv hook fish)
+
+if test -d $HOME/.rbenv
+  status --is-interactive; and . (rbenv init -|psub)
+end
+
+if test -d $HOME/.pyenv
+  status --is-interactive; and . (pyenv init -|psub)
+end
+
+if test -d $HOME/.nvm
+  load_node
+end
+
+sdk list >/dev/null
